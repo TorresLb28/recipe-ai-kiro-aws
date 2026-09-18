5249d0f923e341e1a4b2bfa8a8614eaefd059303
Edgar Torres <edgarlb28@outlook.com>
2026-09-18 17:08:15 -0600

    feat: add S3 + CloudFront construct with OAC (task 5)

--- lib/recipe-infrastructure-stack.ts
+++ lib/recipe-infrastructure-stack.ts
@@ -1,11 +1,15 @@
 ﻿import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
 import * as path from 'path';
 
 export class RecipeInfrastructureStack extends cdk.Stack {
   public readonly recipeFunction: lambda.Function;
+  public readonly distribution: cloudfront.Distribution;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -43,8 +47,67 @@ export class RecipeInfrastructureStack extends cdk.Stack {
       resources: [`arn:aws:logs:us-east-1:*:log-group:/aws/lambda/recipe-ai-*`]
     }));
 
+    // Task 5: Create S3 bucket and CloudFront distribution
+    // Fulfills REQ-9.7, REQ-9.8
+
+    // S3 bucket for static website files with public access blocked (REQ-9.7)
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: `recipe-ai-frontend-${this.account}`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    // Origin Access Control for secure S3 access via CloudFront only
+    const oac = new cloudfront.S3OriginAccessControl(this, 'OAC', {
+      signing: cloudfront.Signing.SIGV4_ALWAYS
+    });
+
+    // CloudFront distribution serving S3 bucket via OAC (REQ-9.8)
+    this.distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket, {
+          originAccessControl: oac
+        }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        compress: true,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.seconds(0)
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.seconds(0)
+        }
+      ],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100
+    });
+
+    // S3 bucket policy: Allow CloudFront OAC to read objects
+    websiteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+      actions: ['s3:GetObject'],
+      resources: [websiteBucket.arnForObjects('*')],
+      conditions: {
+        StringEquals: {
+          'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${this.distribution.distributionId}`
+        }
+      }
+    }));
+
     // Stack resources will be added in subsequent tasks:
-    // - S3 bucket and CloudFront distribution (Task 5)
     // - HTTP API Gateway (Task 6)
     // - Stack outputs (Task 7)
   }
