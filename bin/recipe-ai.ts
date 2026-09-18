4e9899e1f1571a62ff3bc2f164d2ab24feddbd22
Edgar Torres <edgarlb28@outlook.com>
2026-09-18 16:37:56 -0600

    feat: CDK stack skeleton (task 2)

---
+++ bin/recipe-ai.ts
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { RecipeInfrastructureStack } from '../lib/recipe-infrastructure-stack';
+
+const app = new cdk.App();
+
+new RecipeInfrastructureStack(app, 'RecipeAiStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'us-east-1'
+  }
+});
+
+app.synth();
---
+++ lib/recipe-infrastructure-stack.ts
@@ -0,0 +1,14 @@
+﻿import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class RecipeInfrastructureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Stack resources will be added in subsequent tasks:
+    // - Lambda function (Task 4)
+    // - S3 bucket and CloudFront distribution (Task 5)
+    // - HTTP API Gateway (Task 6)
+    // - Stack outputs (Task 7)
+  }
+}
